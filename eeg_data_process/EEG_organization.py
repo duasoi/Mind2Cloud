54a9f2d246b0af3d018627edc9c31ffc7d0aabd8
常安 <138177548+duasoi@users.noreply.github.com>
2025-08-20 16:32:09 +0800

    Add files via upload

---
+++ eeg_data_process/EEG_organization.py
@@ -0,0 +1,45 @@
+import os
+import numpy as np
+###
+def data_organization(sub='sub04'):
+    root_path = '/data1/hxf/neuro-3D-main/EEG_3Datasets/EEGdata/'
+    print(root_path)
+    npy_name_list = ['1s_250Hz.npy', '6s_100Hz.npy']
+    for npy_name in npy_name_list:
+        save_arr_name = f"{root_path}{sub}/process_data_{npy_name}"
+        label_arr_name = f"{root_path}{sub}/name_label.npy"
+        if os.path.exists(save_arr_name):
+            data_array = np.load(save_arr_name)
+            label_arr = np.load(label_arr_name)
+        else:
+            print('error')
+            return
+        print(sub)
+        print(data_array.shape, label_arr.shape)  # (1728, 64, 250) (1728,)
+        result_dir = {}
+        for ii in range(0, data_array.shape[0]):
+            name = label_arr[ii]
+            if name in result_dir.keys():
+                result_dir[name].append(data_array[ii:ii+1])
+            else:
+                result_dir[name] = [data_array[ii:ii+1]]
+        sort_keys = sorted(list(result_dir.keys()))
+        print(sort_keys)
+        train_list, test_list, train_label, test_label = [], [], [], []
+        for key in sort_keys:
+            data_arr_one_cls = np.concatenate(result_dir[key], axis=0)
+            if key[-2:] == '08' or key[-2:] == '09':
+                print(data_arr_one_cls.shape)  # (4, 64, 250)
+                test_list.append(data_arr_one_cls[np.newaxis, :, :, :])
+                test_label.append(key)
+            else:
+                train_list.append(data_arr_one_cls[np.newaxis, :, :, :])
+                train_label.append(key)
+        # import pdb;pdb.set_trace()
+        train_data = np.concatenate(train_list, axis=0).reshape(72, 8, 2, 64, data_array.shape[-1])
+        test_data = np.concatenate(test_list, axis=0).reshape(72, 2, 4, 64, data_array.shape[-1])
+        np.save(f"{root_path}{sub}/{sub}_train_data_{npy_name}", train_data)
+        np.save(f"{root_path}{sub}/{sub}_test_data_{npy_name}", test_data)
+
+if __name__ == '__main__':
+    data_organization(sub='sub04')
---
+++ eeg_data_process/modulation_loader.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import time
+import logging
+import os
+import random
+import torch
+import torch.utils.data
+from diff_utils.helpers import *
+import fnmatch
+import random
+
+import pandas as pd
+import numpy as np
+import csv, json
+
+from tqdm import tqdm
+
+
+class ModulationLoader(torch.utils.data.Dataset):
+    def __init__(self, data_path, context_path=None, split_file=None, pc_size=None):
+        super().__init__()
+
+        self.conditional = context_path is not None
+
+        if self.conditional:
+            self.modulations, self.condition_paths = self.load_modulations(data_path, context_path, split_file)
+        else:
+            self.modulations = self.unconditional_load_modulations(data_path, split_file)
+        # self.modulations = self.modulations[0:8]
+        # context_paths = context_paths[0:8]
+
+        print("data shape, dataset len: ", self.modulations[0].shape, len(self.modulations))
+        # assert args.batch_size <= len(self.modulations)
+
+        # assert len(self.condition_paths) == len(self.modulations)
+
+    def __len__(self):
+        return len(self.modulations)
+
+    def __getitem__(self, index):
+
+        if self.conditional:
+            context = self.condition_paths[index]
+            c = random.choice(context)
+            return {
+                "context": c,
+                "latent": self.modulations[index]
+            }
+        else:
+            return {
+                # "context": None,
+                "latent": self.modulations[index]
+            }
+
+    def load_modulations(self, data_source, pc_source, split, f_name="latent.txt", add_flip_augment=False,
+                         return_filepaths=True):
+        files = []
+        filepaths = {}
+        length = len(os.listdir(data_source))
+        for idx in range(length):
+            instance_filename = os.path.join(data_source, str(idx), f_name)
+
+            files.append(torch.from_numpy(np.loadtxt(instance_filename)).float())
+            tmp = []
+            for filename in os.listdir(os.path.join(data_source, str(idx))):
+                if fnmatch.fnmatch(filename, "text*"):
+                    condition_filename = os.path.join(data_source, str(idx), filename)
+                    tmp.append(torch.from_numpy(np.loadtxt(condition_filename)).unsqueeze(0).float())
+            filepaths[idx] = tmp
+        return files, filepaths
+
+    def unconditional_load_modulations(self, data_source, split, f_name="latent.txt", add_flip_augment=False):
+        files = []
+        length = len(os.listdir(data_source))
+        for idx in range(length):
+            instance_filename = os.path.join(data_source, str(idx), f_name)
+            # print("instance_filename", instance_filename)
+            files.append(torch.from_numpy(np.loadtxt(instance_filename)).float())
+        return files
